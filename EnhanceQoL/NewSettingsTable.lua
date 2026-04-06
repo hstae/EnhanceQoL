872bcc0da27210f78c46f5b0302ec1f6bfa1ed1a
R41z0r <philipp.barth@cloudra.de>
2026-04-06 09:37:24 +0200

    removed new tags

--- EnhanceQoL/NewSettingsTable.lua
+++ EnhanceQoL/NewSettingsTable.lua
@@ -1,8 +1,5 @@
 local addonName, addon = ...
 
 addon.variables.NewVersionTableEQOL = {
-	EQOL_UI = true,
-	EQOL_ResourceBars = true,
-	EQOL_totalAbsorbTrackerEnabled = true,
-	EQOL_Nameplates = true,
+
 }
