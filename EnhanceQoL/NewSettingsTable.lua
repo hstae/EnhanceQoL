905d6e1d49c9d3b5dc4540cdbc8ae2265ea917a9
R41z0r <philipp.barth@cloudra.de>
2026-04-30 17:51:36 +0200

    added missing beta gate

--- EnhanceQoL/NewSettingsTable.lua
+++ EnhanceQoL/NewSettingsTable.lua
@@ -1,7 +1,9 @@
 local addonName, addon = ...
 
 addon.variables.NewVersionTableEQOL = {
+	--@eqol-beta@
 	EQOL_GENERAL = true,
 	EQOL_BagsInventory = true,
 	EQOL_enableBagsModule = true,
+	--@end-eqol-beta@
 }
